8119d3d6238e94d5f8178edc78200d5fda4a0955
frost23z <80541095+frost23z@users.noreply.github.com>
2026-09-22 12:30:15 +0600

    refactor(model): extract nested response structs for simpler mapping

--- models/response.go
+++ models/response.go
@@ -4,37 +4,45 @@ type ResponseItem struct {
 	ID        string
 	Feed      int
 	Published bool
-	GeoInfo   struct {
-		Breadcrumbs []string // TODO: Simple string array or stuct with LocationID, Name?
-		City        string
-		Country     string
-		CountryCode string
-		Name        string
-		LocationID  string
-		Lat         float64
-		Lon         float64
-		State       string
-		StateAbbr   *string
-	}
-	Property struct {
-		Amenities    []string
-		Name         string
-		Slug         string
-		PropertyType string
-		Price        float64
-		ReviewScore  float64
-		StarRating   int
-		Counts       struct {
-			Bathroom  int
-			Bedroom   int
-			Reviews   int
-			Occupancy int
-		}
-		Image struct {
-			Count  int
-			Images []string
-		}
-	}
+	GeoInfo   GeoInfo
+	Property  Property
+}
+
+type GeoInfo struct {
+	Breadcrumbs []string // TODO: Simple string array or stuct with LocationID, Name?
+	City        string
+	Country     string
+	CountryCode string
+	Name        string
+	LocationID  string
+	Lat         float64
+	Lon         float64
+	State       string
+	StateAbbr   *string
+}
+
+type Property struct {
+	Amenities    []string
+	Name         string
+	Slug         string
+	PropertyType string
+	Price        float64
+	ReviewScore  float64
+	StarRating   int
+	Counts       PropertyCounts
+	Image        PropertyImage
+}
+
+type PropertyCounts struct {
+	Bathroom  int
+	Bedroom   int
+	Reviews   int
+	Occupancy int
+}
+
+type PropertyImage struct {
+	Count  int
+	Images []string
 }
 
 type Response struct {
